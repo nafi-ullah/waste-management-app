8b4fa33905f8519cf9d7b623782904685dce5244
nafi-ullah <shafinnafiullah@gmail.com>
2024-05-07 00:20:08 +0600

    splash screen

---
+++ lib/constants/theming.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor =  Color(0xFF262b31);
+const kPrimaryColor = Color.fromRGBO(223, 37, 60, 1);
+const ksecondaryHeaderColor = Color(0xFF151a1d);
+String uri= 'http://192.168.0.114:3000';
+
+const appBarGradient = LinearGradient(
+  colors: [
+
+    Color.fromRGBO(223, 37, 60, 1),
+    Color.fromRGBO(223, 37, 60, 0.5745098039215686),
+
+  ],
+  stops: [0.5 ,1.0],
+);
+
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:waste_management/constants/theming.dart';
+import 'package:waste_management/screens/welcome/splashscreen.dart';
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  //final AuthServices authService = AuthServices();
+
+  @override
+  void initState() {
+    super.initState();
+   // authService.getUserData(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Auth Screen 1',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: kBackgroundColor,
+          secondaryHeaderColor: const Color(0xFF151a1d),
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white.withOpacity(.2),
+              ),
+            ),
+          ),
+        ),
+        home:  const SplashScreen()
+    );
+  }
+}
